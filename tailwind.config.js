45c2c768b14185f5f3bef12eabb252aec83390c8
mateus trentz <mateus.trentz@gmail.com>
2021-10-17 17:19:53 -0300

    cards bons, falta responsivo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['Roboto', 'Helvetica', 'sans-serif']
+    },
     extend: {
       backgroundImage: (theme) => ({
         'mozaic-bg': "url('/public/logos/background.png')",
